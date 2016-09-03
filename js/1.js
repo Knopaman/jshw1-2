c1b5c1b642a4d1115e7ac260840b2c9b077150e0
Knopaman <antikor223@gmail.com>
2016-09-03 14:54:36 +0300

    first

---
+++ js/1.js
@@ -0,0 +1,16 @@
+function pow(a,b) {
+    var result=a;
+    for(var i= 1; i < b; ++i){
+        result *= a;
+    }
+    return result;
+}
+var a=prompt('Введите число:');
+var b=prompt('Введите степень:');
+if (b <= 1) {
+    alert('Степень ' + b +
+        'не поддерживается, введите целую степень, большую 1'
+    );
+} else {
+    console.log( pow(a, b) );
+}
---
+++ js/2.js
@@ -0,0 +1,22 @@
+var arr=[];
+var name;
+for(var i=0; i<5; i++) {
+    arr[i]=prompt('Введите имя пользователя:')
+}
+
+name=prompt('Введите имя');
+var l=0;
+for (var i=0; i<5; i++){
+    if (arr[i]==name){
+        l=1;
+    }
+    if (arr[i]==name){
+        alert(name + ', вы успешно вошли');
+        //break
+    }
+}
+
+if (l==0){
+    alert('Ошибка');
+}
+
